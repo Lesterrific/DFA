548903792f6aa9b259057c2608fb847503186f7b
Lester <mark_lester7@hotmail.com>
2016-03-06 17:23:40 +0800

    Added PuzzleState Class

--- CMSC141_MP2/Program.cs
+++ CMSC141_MP2/Program.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CMSC141_MP2 {
+
     class Program {
         static void Main(string[] args) {
+            PuzzleState PS = new PuzzleState();
         }
+    }
+
+    class PuzzleState {
+        List<char> LeftBank;
+        List<char> RightBank;
+
+        public PuzzleState() {
+            LeftBank = new List<char>();
+            RightBank = new List<char>();
+            Reset();
+        }
+
+        public void Reset() {
+            LeftBank.Clear();
+            LeftBank.Add('M');
+            LeftBank.Add('L');
+            LeftBank.Add('R');
+            LeftBank.Add('C');
+            RightBank.Clear();
+        }
+
     }
 }
